3886aa8078e14664597e688197ccfa9ae4a61daf
Emeka Agara <emekaagara@gmail.com>
2023-01-16 17:28:46 +0100

    side bar done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {Navbar,Sidebar} from './components'
 
 const App = () => {
   return (
-    <div className='relative sm:-8 9-4 bg-[#13131a] min-h-screen flex flex-row'>
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
       <div className='sm:flex hidden mr-10 relative'>
         <Sidebar/>
       </div>
