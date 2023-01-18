8619787ff36d1b4219fcef6bea0fd808248d2221
Emeka Agara <emekaagara@gmail.com>
2023-01-18 22:43:14 +0100

    mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,15 +34,28 @@ const Navbar = () => {
 
         <Link to='/profile'>
           <div className='w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
-            <img src={thirdweb} alt='user' classname='w-[70%] h-[50%] object-contain '/>
+            <img src={thirdweb} alt='user' classname='w-[50%] h-[50%] object-contain '/>
           </div>
         </Link>
       </div>
 
       <div className='sm:hidden flex justify-between items-center relative'>
-        <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+        <div className='w-[40px] h-[40px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
           <img src={thirdweb} alt='user' classname='w-[70%] h-[50%] object-contain '/>
         </div>
+
+        <img
+          src={menu}
+          alt='menu'
+          className='w-[34px] h-[34px] object-contain cursor-pointer'
+          onClick={() => setToggleDrawer(!toggleDrawer)}
+        />
+
+        <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer} ? '-translate-y-[100vh]' : 'translate-y-0]' transition-all duration-700` }>
+
+        </div>
+
+      
       </div>
 
 
