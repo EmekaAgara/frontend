a542efd16d27ff2cd421e2316e222bed2fcf1ef9
Emeka Agara <emekaagara@gmail.com>
2023-01-23 14:49:16 +0100

    create campaign done

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -55,6 +55,36 @@ const CreateCampaign = () => {
             value={form.description}
             handleChange={() => {}}
           />
+        <div className='w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]'>
+          <img src={money} alt="money"className='w-[40px] h-[40px] object-contsin'/>
+          <h4 className='font-epilogue font-bold text-[25px] text-white ml-[20px]'>You will get 100% of all the money raised</h4>
+        </div>
+
+        <div className='flex flex-wrap gap-[40px]'>
+          <FormField
+            labelName="Your Goal *"
+            placeholder="ETH 0.20"
+            inputType="text"
+            value={form.target}
+            handleChange={() => {}}
+          />
+
+          <FormField
+            labelName="End Date *"
+            placeholder="End Date"
+            inputType="date"
+            value={form.deadline}
+            handleChange={() => {}}
+          />
+
+          <div className='flex justify-center items-center mt-[40px]'>
+            <CustomButton
+            btnType="submit"
+            title="Submit new campaign"
+            styles="bg-[#1dc071]"
+          />
+          </div>
+        </div>
       </form>
     </div>
   )
