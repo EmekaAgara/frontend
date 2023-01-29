46a7359d24d844e95398f6ff25d85c5f083396bc
Emeka Agara <emekaagara@gmail.com>
2023-01-29 19:05:29 +0100

    parse campaigns

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -29,6 +29,23 @@ export const StateContextProvider = ({ children }) => {
         }
     }
 
+    const getCampaigns = async () => {
+        const campaigns = await contract.call
+        ('getCampaigns');
+
+        const parsedCampaigns = campaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title:campaign.title,
+            description:campaign.description,
+            target:ethers.utils.formatEther(campaign.target.toString()),
+            image:campaign.image,
+            pId:i
+
+        }));
+
+        return parsedCampaigns;
+    }
+
     return (
         <StateContext.Provider
             value={{
@@ -36,6 +53,7 @@ export const StateContextProvider = ({ children }) => {
                 contract,
                 connect,
                 createCampaign: publishCampaign,
+                getCampaigns
             }}
         >
             {children}
